cf23a9fd63926c3f703e3795a96c346b5d2b699c
diynevala <riku@nevala.info>
2018-10-20 17:43:07 +0300

    Update hubble.js

--- hubble.js
+++ hubble.js
@@ -1,18 +1,18 @@
+feed=false;
 $(function(){
   console.log("hubble loaded and jquery ok.");
-
-  Hubble={
-    this.index = 0;
-    init: function() {
-      console.log('init() called.');
       $.get( 'gallery.rss', function(data) {
-        this.feed = data;
+        console.log("feed loaded.");
+        feed = data;
       });
-    },
-    
-    next: function() {
-      console.log('next() called.');
-    },
-  }
-  
+
 });
+function nextImage(){
+  if(feed){
+    test=feed;
+  }
+  else{
+    console.log("no feed.");
+    return false;
+  }
+}
