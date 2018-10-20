684b877eb4bb551b5429092ae4505eb1b4073ea1
diynevala <riku@nevala.info>
2018-10-20 17:33:09 +0300

    stub functions

--- hubble.js
+++ hubble.js
@@ -1,3 +1,18 @@
 $(function(){
   console.log("hubble loaded and jquery ok.");
+
+  Hubble={
+    this.index = 0;
+    init: function() {
+      console.log('init() called.');
+      $.get( 'gallery.rss', function(data) {
+        this.feed = data;
+      });
+    },
+    
+    next: function() {
+      console.log('next() called.');
+    },
+  }
+  
 });
